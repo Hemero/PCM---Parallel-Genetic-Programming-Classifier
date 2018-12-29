65679d48f92db3772fba36a9f71d7b269e14cbf2
Ana Fidalgo <fc49034@alunos.fc.ul.pt>
2018-12-29 22:39:07 +0000

    measure fitness method

--- genetic-algorithm/src/main/java/pcm/genetic_algorithm/Main.java
+++ genetic-algorithm/src/main/java/pcm/genetic_algorithm/Main.java
@@ -5,24 +5,72 @@ import net.objecthunter.exp4j.Expression;
 
 public class Main {
 
+	private static double[][] data;
+	private static Expression express;
+	private static String[] variables;
+
+	private static final double THRESHOLD = 0;
+
+	private static void loadData(String fileName) {
+		// TODO
+	}
+
+	private static double measureFitness(Expression express) {
+
+		int correctlyClassified = 0;
+
+		for (int row = 0; row < data.length; row++) {
+
+			setVariablesExpression(row, express);	
+
+			try {
+				if ((express.evaluate() < THRESHOLD && data[row][data[0].length-1] < THRESHOLD) || 
+						(express.evaluate() >= THRESHOLD && data[row][data[0].length-1] >= THRESHOLD)) {
+
+					correctlyClassified++;
+				}
+			} catch (ArithmeticException ae) {
+				// Do nothing - counts as incorrectly classified
+			}
+		}
+
+		return 1.0*correctlyClassified / data.length;
+
+	}
+
+	private static void setVariablesExpression(int row, Expression express) {
+
+		for (int col = 0; col < (data[0].length-1); col++) {
+			express.setVariable(variables[col], data[row][col]);
+		}
+
+	}
+
 	public static void main( String[] args ) {
-		
-		String[] variables = {"x", "y", "z"};
-		
+
+		variables = new String[] {"x", "y", "z"};
+
 		ExpressionTree tree = new ExpressionTree(variables);
-		
+
 		tree.generateTree();
-		
+
 		ExpressionTree tree2 = tree.clone();
-		
+
 		System.out.println(tree);
 		System.out.println(tree2);
-		
-		Expression express = tree.getExpression();
+
+		express = tree.getExpression();
 		express.setVariable("x", 1.0);
 		express.setVariable("y", 1.0);
 		express.setVariable("z", 1.0);
+
+		//		System.out.println(express.evaluate());
+
+		data = new double[][] {{0.2,0.3,0.3,0.2},{5.6,0.124,2348.3,5.6},{123.0,124.223,12.3214,123.0}};
+
+		double fitness = measureFitness(express);
 		
-		System.out.println(express.evaluate());
+		tree.setFitness(fitness);
+		System.out.println(fitness);
 	}
 }
