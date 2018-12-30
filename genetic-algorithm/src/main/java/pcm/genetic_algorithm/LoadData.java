6463f4ac13b681402b0c5b8bd2d028271a40a03a
Ana Fidalgo <fc49034@alunos.fc.ul.pt>
2018-12-30 14:10:03 +0000

    add dataOutput for row targets

--- genetic-algorithm/src/main/java/pcm/genetic_algorithm/LoadData.java
+++ genetic-algorithm/src/main/java/pcm/genetic_algorithm/LoadData.java
@@ -1,6 +1,5 @@
 package pcm.genetic_algorithm;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
@@ -11,18 +10,24 @@ public class LoadData {
 
 	// Atributos
 	private double[][] data;
+	private double[] dataOutput;
 	private String[] variables;
+	private double[] classes;
 
 	/**
 	 * Constructor
 	 * @param fileName - String equals to csv file name
+	 * @param classes - the possible classes
 	 * @requires csv file must have name of columns in the firts row
-	 * 			 target must be the last column
-	 * 			 values must be doubles
+	 * 			 values for the attributes must be doubles
+	 * 			 classes must be a 2dim array
+	 * 			 the last column of the csv must only have values from classes array
 	 */
-	public LoadData(String fileName) {
+	public LoadData(String fileName, double[] classes) {
 
 		try {
+			
+			this.classes = classes;
 			
 			CSVReader reader = new CSVReader(new FileReader(fileName));
 			List<String[]> myEntries = reader.readAll();
@@ -32,14 +37,21 @@ public class LoadData {
 			
 			data = new double[numRows][rowLength];
 			
+			// populate data
 			for (int i = 1; i < numRows; i++) {
-				for (int j = 0; j < rowLength; j++) {
+				for (int j = 0; j < (rowLength-1); j++) {
 					data[i][j] = Double.parseDouble(myEntries.get(i)[j]);
 				}
 			}
 			
-			for (int j = 0; j < rowLength; j++) {
+			// populate variables
+			for (int j = 0; j < (rowLength-1); j++) {
 				variables[j] = myEntries.get(0)[j];
+			}
+			
+			// populate output
+			for (int i = 1; i < numRows; i++) {
+				dataOutput[i] = Double.parseDouble(myEntries.get(rowLength-1)[i]);
 			}
 			
 			reader.close();
@@ -48,6 +60,22 @@ public class LoadData {
 			
 			e.printStackTrace();
 		}
+	}
+
+	public double[] getDataOutput() {
+		return dataOutput;
+	}
+
+	public void setDataOutput(double[] dataOutput) {
+		this.dataOutput = dataOutput;
+	}
+
+	public double[] getClasses() {
+		return classes;
+	}
+
+	public void setClasses(double[] classes) {
+		this.classes = classes;
 	}
 
 	public double[][] getData() {
