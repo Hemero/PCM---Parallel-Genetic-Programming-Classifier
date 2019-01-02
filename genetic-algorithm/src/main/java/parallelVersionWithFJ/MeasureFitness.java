1f4d241ba64c1ef91fd455dc2491934bd225ebf4
Paulo Santos <paulo.canelas.santos@hotmail.com>
2019-01-02 20:08:53 +0000

    Fixed MeasureFitness and MeasureExpression

--- genetic-algorithm/src/main/java/parallelVersionWithFJ/MeasureFitness.java
+++ genetic-algorithm/src/main/java/parallelVersionWithFJ/MeasureFitness.java
@@ -4,12 +4,10 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.RecursiveAction;
 
 import abstractsyntaxtree.ExpressionTree;
-import net.objecthunter.exp4j.Expression;
 
 public class MeasureFitness extends RecursiveAction {
 
 	private static final int LIMIT = 2;
-	private static final double THRESHOLD = 0;
 	
 	private ExpressionTree[] population;
 	private String[] variables;
@@ -35,19 +33,17 @@ public class MeasureFitness extends RecursiveAction {
 	protected void compute() {
 		
 		if (end-beg < LIMIT) {
-			
+
 			for (int i = beg; i < end; i++) {
+				MeasureExpression measureExpression =
+					new MeasureExpression(population[i], data, dataOutput, classes, variables);
+				measureExpression.compute();
 				
-				measureExpression(population[i]);
-				
-//				MeasureExpression measureExpression =
-//					new MeasureExpression(population[i], data, dataOutput, classes, variables, 0, this.data.length);
-//				measureExpression.compute();
 			}
 		} else {
 
 			int middle = (end+beg) / 2;
-			
+		
 			MeasureFitness action1 = 
 					new MeasureFitness (population, data, dataOutput, classes, variables, beg, middle);
 			MeasureFitness action2 = 
@@ -55,49 +51,7 @@ public class MeasureFitness extends RecursiveAction {
 			
 			action1.fork();
 			action2.compute();
-			
-			try {
-				action1.get();
-			} catch (InterruptedException | ExecutionException e) {
-				e.printStackTrace();
-			}
+			action1.join();
 		}
 	}
-	
-	private double measureExpression(ExpressionTree tree) {
-
-		Expression express = tree.getExpression();
-		int correctlyClassified = 0;
-
-		for (int row = 0; row < data.length; row++) {
-
-			setVariablesExpression(row, express);	
-
-			try {
-				double expressionEvaluation = express.evaluate();
-				
-				if ((expressionEvaluation < THRESHOLD && dataOutput[row] == classes[0]) || 
-					(expressionEvaluation >= THRESHOLD && dataOutput[row] == classes[1])) {
-
-					correctlyClassified++;
-				}
-			} catch (ArithmeticException ae) {
-				// Do nothing - counts as incorrectly classified
-			}
-		}
-		
-		double fitness = 1.0*correctlyClassified / data.length;
-		
-		tree.setFitness(fitness);
-		
-		return fitness;
-	}
-	
-	private void setVariablesExpression(int row, Expression express) {
-
-		for (int col = 0; col < data[row].length; col++) {
-			express.setVariable(variables[col], data[row][col]);
-		}
-	}
-
 }
